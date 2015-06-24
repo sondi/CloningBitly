40eac76c5c83cabb45e7c3b1736f80919cea8bca
codeamac02 <codeamac02@gmail.com>
2015-06-24 17:42:37 -0500

    Short URL method and displays to user

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,27 +1,21 @@
 get '/' do
   # La siguiente linea hace render de la vista 
   # que esta en app/views/index.erb
+  @url_view = Url.all
   erb :index
 end
 
 post '/new_url' do
 	user_url = params[:user_url]
-	url_instance = Url.new(url: user_url)
-	puts url_instance.url
+	url_instance = Url.new(url: user_url, click_count: 0)
 	url_instance.save
-	puts url_instance.id
-	puts url_instance.id
-	p url_instance
+
+	redirect to '/'
 end
 
-post '/urls' do
-
-  # crea una nueva Url
-end
 
 # e.g., /q6bda
 get '/:short_url' do
-  
   # redirige a la URL original
 end
 
--- app/models/url.rb
+++ app/models/url.rb
@@ -3,11 +3,14 @@ class Url < ActiveRecord::Base
 	before_save :create_short_url
 
 	def create_short_url
-		puts "R" * 28
-		puts self.short_url
-		self.short_url = "Algo"
-		puts self.short_url
-     
+		array = []
+		numbers = (1..9).to_a
+		mayus = ("A".."Z").to_a
+		characters = ('a'..'z').to_a + numbers + mayus
+		array << characters.sample(7)
+		string = array.join
+		self.short_url = "http://localhost:9393/#{string}" 
+
   end
   
 end
